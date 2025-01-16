965e29da6dc17072993024df40c82c0bb582b8ac
Justin <jhsu2022@gmail.com>
2025-01-16 15:08:30 -0800

    trying to figure out test bench for part 1

--- lab2/simulation/part1/tb_part1.v
+++ lab2/simulation/part1/tb_part1.v
@@ -1,40 +1,32 @@
 module fullAdder(
 
-input [8:0] A, B,  
-input Cin,
-output [8:0] S, 
-output Cout
+	input [8:0] A, 
+	input [8:0] B, 
+	input [1:0] Cin,
+	output [8:0] S, 
+	output [1:0] Cout
+
 );
 
-assign S = A ^ B ^ Cin;
-assign Cout = (A & B) | (A & Cin) | (B & Cin);
+	assign S = A ^ B ^ Cin;
+	assign Cout = (A & B) | (A & Cin) | (B & Cin);
 
 endmodule
 
 module tb;
-
-reg [8:0] A, B;
-reg Cin;
 wire [8:0] S;
 wire Cout;
-
-//design under test
-
-rippleAdder RA(S[8:0], Cout, A[8:0], B[8:0], Cin);
-
-inital begin
-
-        // Iterate through all possible values of A, B, and Cin
-        for (i = 0; i < 65536; i = i + 1) begin
-            {A, B, Cin} = i;
-            #10; //
-
-            // Display the results
-            $monitor("A = %b, B = %b, Cin = %b | Sum = %b, Cout = %b", A, B, Cin, S, Cout);
-        end
-
-$finish
-
+reg [8:0] test;
+// design under test;
+fullAdder FA (S, Cout, test[1], test[0]);
+// stimulus and verification that the output is correct
+initial begin
+for (test = 0; test < 65536; test = test + 1)
+begin
+#10;
+if ({Cout, S} != (test[1] + test[0]) )
+$display("ERROR: a=%b b=%b sum=%b cout=%b", test[1], test[0], S, Cout);
+end
+#10 $finish;
+end
 endmodule
-
-
