fbdf7c4d925fe78504899384c48025be7f8ca3f2
Justin <jhsu2022@gmail.com>
2025-01-27 17:02:36 -0800

    lab3 part 2 fixed some stuff and made neater

--- lab3/simulation/part 2/tb_part2.v
+++ lab3/simulation/part 2/tb_part2.v
@@ -1,12 +1,12 @@
 module lzd (
-    output reg [2:0] z, // 3-bit output to represent leading zero count
-    input [3:0] i      // 4-bit input
+    output reg [2:0] z, 
+    input [3:0] i     
 );
 
     always @(*) begin
-        z[2] = (~i[3] & ~i[2] & ~i[1] & ~i[0]); // All bits are 0
-        z[1] = (~i[3] & ~i[2] & i[0]) | (~i[3] & ~i[2] & i[1]); // Position 2 or 1
-        z[0] = (~i[3] & i[2]) | (~i[3] & ~i[1] & i[0]); // Position 3 or 2
+        z[2] = (~i[3] & ~i[2] & ~i[1] & ~i[0]);
+        z[1] = (~i[3] & ~i[2] & i[0]) | (~i[3] & ~i[2] & i[1]);
+        z[0] = (~i[3] & i[2]) | (~i[3] & ~i[1] & i[0]);
     end
 
 endmodule
@@ -34,7 +34,6 @@ module lzd8b (
     always @(*) begin
         if (z_upper == 3'd4) begin
             
-            // If the upper 4 bits are all zero, add zeros from both halves
             z8 = z_upper + z_lower;
 
         end 
@@ -44,9 +43,9 @@ module lzd8b (
             z8 = z_upper;
         end
 
-        // Special case for all zeros
+        //check for edge case
         if (i8 == 8'b00000000) begin
-            z8 = 4'b0000; // Set 8 leading zeros for all-zero input
+            z8 = 4'b0000;
         end
     end
 
@@ -67,9 +66,9 @@ module testbench;
     initial begin
         
         for (j = 0; j < 10; j = j + 1) begin
-            i8 = $random; // Generate a random 8-bit value
+            i8 = $random;
             
-            #10;          // Wait for output to stabilize
+            #10;   
            
             $display("Input: %b, Leading Zeros: %0d", i8, z8);
        
