083eefa169baa56c512ad73a2145936d1e52d944
0atMeAlready <jhsu2022@gmail.com>
2025-01-30 21:09:49 -0800

    organizational changes

--- lab3/simulation/part 1/tb_part1.v
+++ lab3/simulation/part 1/tb_part1.v
@@ -1,22 +1,3 @@
-module lzd (
-    
-    output reg [3:0] z,
-    input [3:0] i
-
-    );
-
-    always @ (i)
-        begin
-            
-            z[3] = 1'b0;
-            z[2] = (~i[3] * ~i[2] * ~i[1] * ~i[0]);
-            z[1] = (~i[3] * ~i[2] * i[0] | ~i[3] * ~i[2] * i[1]);
-            z[0] = (~i[3] * i[2] | ~i[3] * ~i[1] * i[0]);
-
-        end
-
-endmodule
-
 module testbench;
     
     reg [3:0] i;
