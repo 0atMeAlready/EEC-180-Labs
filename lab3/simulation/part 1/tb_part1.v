36d2f7af4ea4e04cc1dc0af95257f9056ae7fa1e
0atMeAlready <jhsu2022@gmail.com>
2025-02-04 22:24:40 -0800

    something for lab 3 idk

--- lab3/simulation/part 1/tb_part1.v
+++ lab3/simulation/part 1/tb_part1.v
@@ -3,7 +3,6 @@ module testbench;
     reg [3:0] i;
     wire [3:0] z;         
 
-    // Instantiate the design under test (DUT)
     lzd lzd0 (
         
         .i(i),
@@ -15,12 +14,11 @@ module testbench;
 
     initial begin
 
-        // Iterate through all possible combinations of A and B
         for (j = 0; j < 16; j = j + 1) begin
                 
-		i [3:0] = j; // Assign values to A
+		i [3:0] = j;
 
-            #10; // Wait 10 time units for results to stabilize
+            #10;
 
         $display("input = %b, number of leading zeroes = %b | %d", i, z, z);
 
